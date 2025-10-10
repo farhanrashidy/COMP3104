e8b936206427bcef2e70f81e2592270bf1e12944
Sheikh Mohammad Farhan Rashidy <134596484+farhanrashidy@users.noreply.github.com>
2025-10-10 12:06:03 -0400

    Dep inclusions (#3)
    * Github action for github pages added
    
    * name change and course added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101512659 || Sheikh Mohammad Farhan Rashidy || DevOps</h5>
+      <h5>Developed By: 101512659 || Mr. Sheikh Mohammad Farhan Rashidy || COMP3104 || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
